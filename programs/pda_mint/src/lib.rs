b86f3b2364d0e8438c11f6b4cd486dbcc339fb0a
Nathan Shirley <nshirley22@gmail.com>
2021-10-07 09:45:53 -0400

    successfully freezing the mint

--- programs/pda_mint/src/lib.rs
+++ programs/pda_mint/src/lib.rs
@@ -1,14 +1,109 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::{program::invoke_signed, program_pack::Pack}};
+use spl_token::{instruction::{mint_to_checked, AuthorityType}, state::Mint};
+use anchor_spl::token::{self, Token, SetAuthority};
+
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+const VAULT_PDA_SEED: &[u8] = b"authority";
 
 #[program]
 pub mod pda_mint {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn mint_one(ctx: Context<MintOne>) -> ProgramResult {
+        //charge a small fee? -- idk maybe not on this one. enough to discourage u from doing it for no reason. idk
+        //verify that the owner actually owns the token account
+
+        let (_pda, bump_seed) = Pubkey::find_program_address(&[VAULT_PDA_SEED], ctx.program_id);
+        let seeds = &[&VAULT_PDA_SEED[..], &[bump_seed]];
+
+        let mint_infos = vec![
+            ctx.accounts.mint.clone(),
+            ctx.accounts.token_account.clone(),
+            ctx.accounts.mint_auth.clone(),
+            ctx.accounts.mint_auth.clone(),
+        ];
+        //https://docs.rs/spl-token/3.2.0/src/spl_token/instruction.rs.html#1075-1103
+        let mint_instruction = mint_to_checked(
+            ctx.accounts.token_program.key,
+            ctx.accounts.mint.key,
+            ctx.accounts.token_account.key,
+            ctx.accounts.mint_auth.key,
+            &[ctx.accounts.mint_auth.key],
+            1,
+            0
+        )?;
+        invoke_signed(
+            &mint_instruction, 
+            mint_infos.as_slice(), 
+            &[&seeds[..]]
+        )?;
+
+        //example calling another program in your program
+        let mut tester_accounts = Tester {
+            test: ctx.accounts.mint_auth.clone()
+        };
+        let tester_context = Context::new(ctx.program_id, & mut tester_accounts, &[]);
+        tester(tester_context)?;
+        
+        //check supply -- if greater than 7, freeze
+        let mint_supply = Mint::unpack(&ctx.accounts.mint.data.borrow())?.supply;
+        if mint_supply > 0 {
+            freeze_mint_supply(ctx, seeds)?;
+        }
+        Ok(())
+    }
+
+    pub fn tester(ctx: Context<Tester>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintOne<'info> {
+    #[account(mut)]
+    mint: AccountInfo<'info>,
+    mint_auth: AccountInfo<'info>,
+    #[account(mut)]
+    token_account: AccountInfo<'info>,
+    token_program: Program<'info, Token>,
+    owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Tester<'info> {
+    test: AccountInfo<'info>
+}
+
+impl<'info>MintOne<'info> {
+    fn into_freeze_mint_supply_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            current_authority: self.mint_auth.clone(),
+            account_or_mint: self.mint.clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+
+fn freeze_mint_supply(ctx: Context<MintOne>, seeds: &[&[u8]; 2]) -> ProgramResult {
+    token::set_authority(
+        ctx.accounts.into_freeze_mint_supply_context()
+        .with_signer(&[&seeds[..]]),
+        AuthorityType::MintTokens,
+        None
+    )?;
+    //not sure if i'm doing this right
+    emit!(FreezeMint {
+        mint: *ctx.accounts.mint.key,
+        label: "this mint has been frozen".to_string(),
+    });
+    Ok(())
+}
+
+#[event]
+pub struct FreezeMint {
+    pub mint: Pubkey,
+    #[index]
+    pub label: String,
+}
--- tests/pda_mint.js
+++ tests/pda_mint.js
@@ -1,14 +1,133 @@
 const anchor = require('@project-serum/anchor');
+const { rpc } = require('@project-serum/anchor/dist/cjs/utils');
+const { Keypair, PublicKey, TransactionInstruction, SystemProgram } = anchor.web3;
+const SPL = require("@solana/spl-token");
+const { TOKEN_PROGRAM_ID, Token, MintLayout }  = SPL;
 
 describe('pda_mint', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env()
+  anchor.setProvider(provider);
+  const program = anchor.workspace.PdaMint;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.PdaMint;
-    const tx = await program.rpc.initialize();
+  let mint = Keypair.generate();
+  let payer = provider.wallet.payer;
+  let payerTokenAccount = null;
+  let rent = null;
+  let authPda = null;
+
+  it('config', async () => {
+    const _rent = await provider.connection.getMinimumBalanceForRentExemption(
+      MintLayout.span
+    );
+    rent = _rent;
+    let [_authPDA, nonce] = await PublicKey.findProgramAddress(
+      //gets a determinstic pda address using this string and the program id
+      [anchor.utils.bytes.utf8.encode("authority")],
+      program.programId
+    );
+    authPda = _authPDA;
+
+    payerTokenAccount = await getAssociatedTokenAccountAddress(payer.publicKey, mint.publicKey);
+  });
+
+  it('mint one token', async () => {
+    const tx = await program.rpc.mintOne({
+      accounts: {
+        mint: mint.publicKey,
+        mintAuth: authPda,
+        tokenAccount: payerTokenAccount,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        owner: payer.publicKey,
+      }, instructions: [
+        SystemProgram.createAccount({
+          fromPubkey: payer.publicKey,
+          newAccountPubkey: mint.publicKey,
+          space: MintLayout.span,
+          lamports: rent,
+          programId: TOKEN_PROGRAM_ID
+        }),
+        // https://github.com/solana-labs/solana-program-library/blob/c371995c86d1ec21e5a44a76ed27152389024a2e/token/js/client/token.js#L1425
+        Token.createInitMintInstruction(
+          TOKEN_PROGRAM_ID,
+          mint.publicKey,
+          0,
+          authPda,
+          authPda,
+        ),
+        createAssociatedTokenAccountInstruction(
+          mint.publicKey,
+          payerTokenAccount,
+          payer.publicKey,
+          payer.publicKey,
+        ),
+      ], 
+      signers: [
+        payer, mint, 
+      ]
+    });
     console.log("Your transaction signature", tx);
+
+    //i need to bring the create mint into the program
+    // const again = await program.rpc.mintOne({
+    //   accounts: {
+    //     mint: mint.publicKey,
+    //     mintAuth: authPda,
+    //     tokenAccount: payerTokenAccount,
+    //     tokenProgram: TOKEN_PROGRAM_ID
+    //   },
+    // });
+    // console.log("Your transaction signature", again);
+  });
+
+  it('see if it worked', async () => {
+    let TokenMint = new Token(
+      provider.connection,
+      mint.publicKey,
+      TOKEN_PROGRAM_ID,
+      payer
+    );
+    let info = await TokenMint.getAccountInfo(payerTokenAccount);
+    //console.log(info.amount)
+    let balance = await provider.connection.getTokenAccountBalance(payerTokenAccount);
+    console.log("the user's token balance: ", balance);
+    let mintInfo = await TokenMint.getMintInfo();
+    console.log("the mint's supply: ", mintInfo.supply);
   });
 });
+
+
+function createAssociatedTokenAccountInstruction(
+  mint,
+  associatedAccount,
+  owner,
+  payer,
+) {
+  const data = Buffer.alloc(0);
+  let associatedProgramId = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
+  let programId = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+  let keys = [
+    {pubkey: payer, isSigner: true, isWritable: true},
+    {pubkey: associatedAccount, isSigner: false, isWritable: true},
+    {pubkey: owner, isSigner: false, isWritable: false},
+    {pubkey: mint, isSigner: false, isWritable: false},
+    {pubkey: SystemProgram.programId, isSigner: false, isWritable: false},
+    {pubkey: programId, isSigner: false, isWritable: false},
+    {pubkey: anchor.web3.SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
+  ];
+  return new TransactionInstruction({
+    keys,
+    programId: associatedProgramId,
+    data,
+  });
+}
+async function getAssociatedTokenAccountAddress(owner, mint) {
+  let associatedProgramId = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
+  return (
+    await PublicKey.findProgramAddress(
+      [owner.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mint.toBuffer()],
+      associatedProgramId
+    )
+  )[0];
+};
