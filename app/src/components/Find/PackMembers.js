7d3f80a966c8a6925fdace4315ad9d8e7efa4f31
Nathan Shirley <nshirley22@gmail.com>
2021-10-15 11:36:18 -0400

    members card look

--- app/src/components/Find/PackMembers.js
+++ app/src/components/Find/PackMembers.js
@@ -1,26 +1,38 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+
+const PackMembers = ({ members }) => {
 
 
-const PackMembers = ({members}) => {
-    let memberItems = null;
     if (members.length > 0) {
-        memberItems = members.map((member, index) => {
+        let memberIndexLabels = [];
+        let memberKeys = [];
+        members.forEach((member, index) => {
             member = member.toBase58();
-            return (<div key={index}>a member is {member}</div>);
-        })
+            memberIndexLabels.push(<div>{index + 1}.</div>);
+            memberKeys.push((
+                <div key={index}>{member}</div>
+            ));
+        });
+        return (
+            <div className="members-header">
+                Members
+            <Container className="members-card">
+                <Row>
+                    <Col className="member-index-labels">
+                        {memberIndexLabels}
+                    </Col>
+                    <Col className="member-key-labels">
+                        {memberKeys}
+                    </Col>
+                </Row>
+            </Container>
+            </div>
+        );
     } else {
-        memberItems = <div></div>
+        return (<div></div>);
     }
 
-   
-  
-    return (
-        <div>
-            {memberItems}
-        </div>
-    );
-
-    
 }
 
 export default PackMembers;
